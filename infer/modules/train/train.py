ae783baaeb81c716efb0b036e2d6455411941cc4
박소이 <iso960@naver.com>
2026-07-30 22:48:24 +0900

    fix: Windows 학습 파이프라인 크래시 3건 수정 (libuv, matplotlib API, torch weights_only)
    - train.py: dist.init_process_group 초기화 시 libuv 미지원 문제 해결
      (os.environ[USE_LIBUV] = 0 추가)
    - utils.py: matplotlib 최신 버전에서 제거된 tostring_rgb() 대신
      buffer_rgba() 사용하도록 스펙트로그램 로깅 함수 수정
    - requirements: torch를 2.5.1+cu118로 고정 (2.7.1의 weights_only=True
      기본값이 hubert_base.pt 로드를 차단하는 문제 회피)

--- infer/modules/train/train.py
+++ infer/modules/train/train.py
@@ -113,6 +113,7 @@ def main():
         n_gpus = 1
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = str(randint(20000, 55555))
+    os.environ["USE_LIBUV"] = "0"
     children = []
     logger = utils.get_logger(hps.model_dir)
     for i in range(n_gpus):
